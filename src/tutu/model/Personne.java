4551828ac4c0caf02ff78fc3d220419a8adce3b0
Unknown <malbert.benoit@gmail.com>
2017-06-03 20:55:48 +0200

    Creation de l'attribut nom de la classe Personne

--- src/tutu/model/Personne.java
+++ src/tutu/model/Personne.java
@@ -1,5 +1,14 @@
 package tutu.model;
 
 public class Personne {
+	private String nom;
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
 }
